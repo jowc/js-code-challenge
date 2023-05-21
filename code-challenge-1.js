f1a3c306a8311ba2bcfebedaca033b6d803c3286
Joseph Chikeme <jchikeme@gmail.com>
2023-05-21 16:04:58 +0100

    Completed the algorithm

--- code-challenge-1.js
+++ code-challenge-1.js
@@ -6,12 +6,24 @@ countNumbers = (word) => {
     let tempItems = word.toLowerCase().split("")
     return tempItems.reduce(
       (acc, cur) => {
+        acc[cur] = acc[cur] ? acc[cur] + 1 : 1
+        if (acc[cur] > acc.max) acc.max = acc[cur]
         return acc
       },
       { max: 1, word }
     )
   })
-  return wordObject
+  let max = 1
+  wordObject.forEach((item) => {
+    if (item.max > max) {
+      max = item.max
+    }
+  })
+  let pickedWords = wordObject.filter((item) => item.max === max)
+  if (pickedWords.length > 1) {
+    return pickedWords.map(({ word, ...item }) => word)
+  }
+  return pickedWords[0]["word"]
 }
 
-console.log(countNumbers("I have a lot of code challenges to work on."))
+console.log(countNumbers("I have a lot of code challenges to worrk oon."))
